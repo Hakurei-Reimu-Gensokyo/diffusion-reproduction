c6b288362b50efdb33c66cbbaf1627902cda1d27
Hakurei-Reimu-Gensokyo <14181150+sekirosdadfs@user.noreply.gitee.com>
2025-02-28 13:10:31 +0800

    add ddpm and model convert

--- sd/decoder.py
+++ sd/decoder.py
@@ -68,7 +68,7 @@ class VAE_Decoder(nn.Sequential):
 
     def __init__(self):
         super().__init__(
-            nn.Conv2d(4,4, kernel_size=1, padding=0),
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
             nn.Conv2d(4, 512, kernel_size=3, padding=1),
             VAE_ResidualBlock(512,512),
             VAE_AttentionBlock(512),
